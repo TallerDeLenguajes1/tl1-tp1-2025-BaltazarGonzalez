e91ab82309c3f63f6f69c45be369baaf8e9ed74a
GonzalezjAlejo <ag2458473@gmail.com>
2025-04-01 01:21:19 -0300

    puntero y direccion memoria

--- tp1_1.c
+++ tp1_1.c
@@ -4,5 +4,13 @@ int main() {
     
     printf("Hola Mundo");
 
+     int num,*puntero;
+    num = 10;
+    puntero = &num;
+    printf("contenido puntero:%d\n",*puntero);
+    printf("direccion memoria almacenada por puntero:%p\n",puntero);
+    printf("direccoin memoria de la variable:%p\n",&num);
+    printf("direccion de memoria del puntero:%p\n"),&puntero;
+    printf("tamaño de memoria usado por la variable:%d bytes",sizeof(num));
     return 0;
 }
