9c141dd02643c60e4afa9b764885b7f652bd7940
BaltazarGonzalez <alejobaltazargonzalez@gmail.com>
2025-04-07 13:01:32 -0300

    funcion void cuadrado

--- tp1_2.c
+++ tp1_2.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 int Cuadrado (int a);
+void Cuadrau (int a);
+
 int main() {
     int num,result;
     puts("un numero entero");
@@ -8,10 +10,14 @@ int main() {
     
     result = Cuadrado(num);
     printf("su numero: %d   al cuadrado:%d",num,result);
-    
+    Cuadrau (num);
 
     return 0;
 }
+
 int Cuadrado (int a){
     return(a*a);
+}
+void Cuadrau (int a){
+    printf("\nCuadrado del numero:%d\n",a*a);
 }
